dbc3a67d64a17e28999dbd0cac7b21344643798b
szhaovas <31874536+szhaovas@users.noreply.github.com>
2026-04-25 17:11:04 -0700

    Update base_xpos_offset for table position

--- robosuite/models/robots/manipulators/mounted_jaco6dof.py
+++ robosuite/models/robots/manipulators/mounted_jaco6dof.py
@@ -44,9 +44,7 @@ class MountedJaco6DOF(ManipulatorModel):
     @property
     def base_xpos_offset(self):
         return {
-            "bins": (-0.5, -0.1, 0),
-            "empty": (-0.6, 0, 0),
-            "table": lambda table_length: (-0.16 - table_length / 2, 0, 0),
+            "table": lambda table_length: (0, -0.45, 0),
         }
 
     @property
