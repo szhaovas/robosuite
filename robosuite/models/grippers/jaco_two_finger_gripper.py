fd074fd8b37c17e60a1ce08270b4200e00d3600e
szhaovas <31874536+szhaovas@users.noreply.github.com>
2026-04-25 17:19:38 -0700

    Fixed Jaco two-finger gripper

--- robosuite/models/grippers/jaco_two_finger_gripper.py
+++ robosuite/models/grippers/jaco_two_finger_gripper.py
@@ -61,7 +61,7 @@ class JacoTwoFingerGripper(JacoTwoFingerGripperBase):
         """
         assert len(action) == self.dof
         self.current_action = np.clip(
-            self.current_action + np.array([-1.0, 1.0]) * self.speed * np.sign(action), -1.0, 1.0
+            self.current_action + np.array([-1.0, -1.0]) * self.speed * np.sign(action), -1.0, 1.0
         )
         return self.current_action
 
